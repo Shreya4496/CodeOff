f34efeda23b3e5d71e6ba78c4abb229efe1ea63b
shreya4496 <shreyagupta4496@gmail.com>
2017-10-27 22:26:29 +0530

    Project structure added

---
+++ EventPromotion/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EventpromotionConfig(AppConfig):
+    name = 'EventPromotion'
---
+++ Preventions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PreventionsConfig(AppConfig):
+    name = 'Preventions'
